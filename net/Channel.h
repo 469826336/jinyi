39a12722d12cae8b75a15f8625e4950ed344d2ae
鶸 <49176155+469826336@users.noreply.github.com>
2019-10-22 22:58:06 +0800

    Update Channel.h

--- net/Channel.h
+++ net/Channel.h
@@ -19,6 +19,7 @@ private:
 	//无符号32位整型
 	__uint32_t events_;
 	__uint32_t revents_;
+	__uint32_t lastEvents_;
 	EventCallBack readHandler_;
 	EventCallBack writeHandler_;
 	EventCallBack errorHandler_;
@@ -29,7 +30,7 @@ public:
 	~Channel();
 	int getFd();
 	void setFd(int fd);
-        void getEvents()
+	void getEvents()
 	{
 		return events_;
 	}
@@ -52,7 +53,7 @@ public:
 	//void handleEvent(TimeStamp receiveTime)
 	void handleEvent()
 	{
-		//为什么events_=0 因为已经有事件被触发了 所以events_已经不重要了吗 是因为拿到事件了就不期望在这段时间内在监控任何事件
+		//为什么events_=0 因为已经有事件被触发了 所以events_已经不重要了吗
 		events_ = 0 ;
 		//这里是挂断并且revents_无事件？
 		if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN))
@@ -83,35 +84,36 @@ public:
 				writeHandler_();
 			}
 		}
+
+		
 	}
-	
-                //void handleError(int fd,int err_num,std::string short_msg);
 
-		//muduo用enableReading更新events_,并且调用update()
-		//update会在哪
-	        //在epoll里面通过timerManager更新？
-		void setEvents(__uint32_t ev)
-		{
-			events_ = ev ;
-		}
+	//void handleError(int fd,int err_num,std::string short_msg);
 
-		void setRevents(__uint32_t ev)
-		{
-			revents_ = ev ;
-		}
+	//muduo用enableReading更新events_,并且调用update()
+	//update会在哪
+	void setEvents(__uint32_t ev)
+	{
+		events_ = ev ;
+	}
 
-         //在改某个fd在epoll中监听的事件时，如果和上次一模一样，那就不用改了，算是个小优化，去掉也没问题 --linya
-		bool isEqualAndUpdateLastEvents()
-		{
-			bool ret(lastEvents_ == events_);
-			lastEvents_ = events_ ;
-			return ret;
-		}
+	void setRevents(__uint32_t ev)
+	{
+		revents_ = ev ;
+	}
 
-                __uint32_t getLastEvents()
-		{
-			return lastEvents_;
-		}
+	//在改某个fd在epoll中监听的事件时，如果和上次一模一样，那就不用改了，算是个小优化，去掉也没问题 --linya
+	bool isEqualAndUpdateLastEvents()
+	{
+		bool ret(lastEvents_ == events_);
+		lastEvents_ = events_ ;
+		return ret;
+	}
+
+	__uint32_t getLastEvents()
+	{
+		return lastEvents_;
+	}
 
 
 
