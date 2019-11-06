db158bdef1a5c3f114058b84a3cdd711a71d3d4d
鶸 <49176155+469826336@users.noreply.github.com>
2019-11-06 23:10:13 +0800

    Update Util.h

--- net/Util.h
+++ net/Util.h
@@ -3,8 +3,15 @@
 // @date    2019/10/31
 // @Email   469826336@qq.com
 #pragma once
+#include<cstdlib>
+#include <string>
 
+ssize_t readn(int fd,std::string &inBuffer,bool &zero);
+ssize_t writen(int fd,void *buff,size_t n);
+ssize_t writen(int fd,std::string &sbuff);
 int socket_bind_listen(int port);
 void handle_for_sigpipe();
 int setSocketNonBlocking(int fd);
 void setSocketNodelay(int fd);
+//void setSocketNoLinger(int fd)
+//void shutDownWR(int fd)
