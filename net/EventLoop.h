374eddf68a55f603942034fd4a61c0bd9ec571fe
鶸 <49176155+469826336@users.noreply.github.com>
2019-11-18 21:46:01 +0800

    Update EventLoop.h

--- net/EventLoop.h
+++ net/EventLoop.h
@@ -10,18 +10,6 @@
 class EventLoop
 {
 public:	
-	typedef std::function<void()> Functor;#pragma once
-#include "base/CurrentThread.h"
-#include "MutexLock.h"
-#include "Epoll.h"
-#include <memory>
-#include<vector>
-#include<functional>
-#include <sys/eventfd.h>
-#include"base/MutexLock.h"
-class EventLoop
-{
-public:	
 	typedef std::function<void()> Functor;
 	EventLoop();
 	~EventLoop();
@@ -51,6 +39,16 @@ public:
 	{
 		poller_->epoll_add(channel,timeout);
 	}
+
+	void shutdown(shared_ptr<Channel> channel)
+	{
+		shutDownWR(channel->getFd());
+	}
+
+	void removeFromPoller(shared_ptr<Channel> channel)
+	{
+		poller_->epoll_del(channel);
+	}
 private:
 
 	bool looping_;//atomic
@@ -69,49 +67,4 @@ private:
 	void wakeup();
 	void handleRead();
 	void doPendingFunctors();
-
-
-
-}
-	EventLoop();
-	~EventLoop();
-	void loop();
-	void quit();
-	void runInLoop(Functor&& cb);//右值引用提高效率，减少复制
-	void queueInLoop(Functor&& cb);
-
-	//判断是否在loop线程中
-	bool isInLoopThread()const
-	{
-		return threadId_ == CurrentThread::tid();
-	}
-
-	void assertInLoopThread()
-	{
-		//没有加头文件<assert.h>
-		assert(isInLoopThread());
-	}
-
-
-private:
-
-	bool looping_;//atomic
-	bool quit_;//atomic
-	bool eventHandling_;//atomic
-	const pid_t threadId_;//pid_t这个类型定义实际上就是int型
-	shared_ptr<Epoll> poller_;//通过shared_ptr间接持有Poller
-	//声明顺序 wakeup_ > wakeupChannel_ 不然构造函数出问题
-	int wakeupFd_;//唤醒用eventfd
-	shared_ptr<Channel> wakeupChannel_;
-	bool callingPendingFunctors_;
-	std::vector<Functor> pendingFunctors_;//暴露给了其他线程，因此用mutex_保护
-	mutable MutexLock mutex_;
-
-
-	void wakeup();
-	void handleRead();
-	void doPendingFunctors();
-
-
-
 }
